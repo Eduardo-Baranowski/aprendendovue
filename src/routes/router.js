c1ea928dc2d06266f4f1f6ed51e67d7d9c0bed6d
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2021-03-19 14:39:20 -0300

    aprendendo vue

---
+++ src/routes/router.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import Home from '@/pages/Home';
+import Videos from '@/pages/Videos';
+import Sobre from '@/pages/Sobre';
+
+const routes = [
+  {
+    path:'/',
+    component:Home
+  },
+  {
+    path:'/videos',
+    component:Videos
+  },
+  {
+    path:'/sobre',
+    component: Sobre
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode:'history'
+});
+
+
+export default router;
